25e3126481cf04161c7c939dccee0ca09d26b2ec
ingenzi <vany.ingenzi@gmail.com>
2025-06-23 13:03:28 +0200

    Added small demo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # lazy_file_populator
 Example of a simple FUSE Go lazy file populator
+
+
+- Install FUSE
+> sudo apt-get install fuse3
+
+- Mkdir mnt
+> mkdir mnt
--- cmd/main.go
+++ cmd/main.go
@@ -3,31 +3,61 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
 )
 
+const fileName = "lazy.data"
+
 type FS struct{}
 
 func (FS) Root() (fs.Node, error) {
-	return File{}, nil
+	return Dir{}, nil
+}
+
+type Dir struct{}
+
+func (Dir) Attr(ctx context.Context, a *fuse.Attr) error {
+	a.Inode = 1
+	a.Mode = os.ModeDir | 0555
+	return nil
+}
+
+func (Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
+	log.Println("Looking up the dir")
+	if name == fileName {
+		return File{}, nil
+	}
+	return nil, syscall.Errno(syscall.ENOENT)
+}
+
+func (Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+	log.Println("Reading the dir")
+	// TODO(vi): Cache file in another folder if already request by os.WriteFile(..)
+	// TODO(vi): Read the real_files when content is local os.ReadFile(..)
+	return []fuse.Dirent{
+		{Inode: 2, Name: fileName, Type: fuse.DT_File},
+	}, nil
 }
 
 type File struct{}
 
 func (File) Attr(ctx context.Context, a *fuse.Attr) error {
-	a.Inode = 1
+	a.Inode = 2
 	a.Mode = 0444 // read-only
-	a.Size = 1024 * 1024 // pretend it's a 1MB file
+	a.Size = 1024 * 1024
 	a.Mtime = time.Now()
 	return nil
 }
 
 func (File) ReadAll(ctx context.Context) ([]byte, error) {
 	log.Println("Lazy fetch triggered: generating dummy data")
-	data := make([]byte, 1024*1024) // 1MB of dummy data
+	data := make([]byte, 1024*1024)
 	for i := range data {
 		data[i] = 'A'
 	}
@@ -36,30 +66,33 @@ func (File) ReadAll(ctx context.Context) ([]byte, error) {
 
 func main() {
 	mountpoint := "./mnt"
-	log.Println("Starting ...")
+	log.Println("Mounting lazyfs at", mountpoint)
 
 	c, err := fuse.Mount(
 		mountpoint,
 		fuse.FSName("lazyfs"),
 		fuse.Subtype("lazyfetch"),
-		fuse.LocalVolume(),
-		fuse.VolumeName("LazySyncthingFS"),
 	)
 	if err != nil {
 		log.Fatalf("Mount failed: %v", err)
 	}
-	defer fuse.Unmount(mountpoint)
 	defer c.Close()
 
-	log.Printf("Mounted at %s", mountpoint)
+	// Handle Ctrl+C for graceful unmount
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
 
-	err = fs.Serve(c, FS{})
-	if err != nil {
+	go func() {
+		<-sigs
+		log.Println("Received signal, unmounting...")
+		if err := fuse.Unmount(mountpoint); err != nil {
+			log.Printf("Failed to unmount: %v", err)
+		}
+		os.Exit(0)
+	}()
+
+	log.Println("Filesystem is now available")
+	if err := fs.Serve(c, FS{}); err != nil {
 		log.Fatalf("Serve failed: %v", err)
-	}
-
-	<-c.Ready
-	if err := c.MountError; err != nil {
-		log.Fatalf("Mount error: %v", err)
 	}
 }
